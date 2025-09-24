abbedabacac42ab9f9a7282bc38e1f7bd92e0303
Avnoor Kaur <Avnoork2004@gmail.com>
2025-09-23 22:06:10 -0400

    navigation logic from page to page

--- app/src/main/java/com/example/breathebetter/NavGraph.kt
+++ app/src/main/java/com/example/breathebetter/NavGraph.kt
@@ -1,2 +1,25 @@
 package com.example.breathebetter
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+
+// defines the nav graph for the app.
+// maps different strings to their screens which are all the files
+@Composable
+fun AppNavHost(navController: NavHostController) {
+    //splash screen shows when the app starts
+    NavHost(navController = navController, startDestination = "splash") {
+        //splash screen
+        composable("splash") { SplashScreen(navController) }
+        //"welcome" choice screen with login and register btns
+        composable("auth") { AuthChoiceScreen(navController) }
+        //registration screen
+        composable("register") { RegistrationScreen(navController) }
+        //login screen
+        composable("login") { LoginScreen(navController) }
+        //homescreen
+        composable("home") { HomeScreen(navController) }
+    }
+}
